c4fc3873fc996101e7aaaf57beea51959129d761
Koushik Mandal <koushikmandal05@gmail.com>
2024-06-24 18:18:06 +0530

    added express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(res.body);
-
-})
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 5 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password must be atleast 8 characters").isLength({min: 8}),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "Please enter a unique email.",
+          message: err.message,
+        });
+      });
+  }
+);
 
 module.exports = router;
