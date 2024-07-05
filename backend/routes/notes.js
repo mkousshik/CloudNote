3fc875ee338f8c18fae2baaaf0694dccd2a1beb1
Koushik Mandal <koushikmandal05@gmail.com>
2024-07-05 20:07:21 +0530

    update and delete note added on backend

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,14 +6,13 @@ const { body, validationResult } = require("express-validator");
 
 // Route 1: get all the notes using: GET "api/notes/fetchallnotes". Login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
-    try {
-        const notes = await Notes.find({ user: req.user.id });
-        res.json(notes);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error")
-    }
-  
+  try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Route 2: add a new notes using: POST "api/notes/addnote". Login required.
@@ -22,7 +21,9 @@ router.post(
   fetchuser,
   [
     body("title", "Enter a valid title").isLength({ min: 3 }),
-    body("description", "Description must be atleast 5 characters").isLength({min: 5 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -31,31 +32,71 @@ router.post(
     }
 
     try {
-        const {title, description, tag} = req.body;
-        const note = new Notes({
-            title, description, tag, user: req.user.id
-        })
-        const saveNote = await note.save();
-        res.json(saveNote);
+      const { title, description, tag } = req.body;
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const saveNote = await note.save();
+      res.json(saveNote);
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error")
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
-
   }
 );
 // Route 3: update a existing note using: PUT "api/notes/updatenote". Login required.
-router.put( "/updatenote",fetchuser, async (req, res) => {
-      const {title, description, tag} = req.body;
-      const newNote = {};
-      if(title) newNote.title = title;
-      if(description) newNote.description = description;
-      if(tag) newNote.tag = tag;
-      
-  
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+
+  try {
+    // create a new note object.
+    const newNote = {};
+    if (title) newNote.title = title;
+    if (description) newNote.description = description;
+    if (tag) newNote.tag = tag;
+
+    // find the note to be updated and updated it.
+    let note = await Notes.findById(req.params.id);
+    if (!note) return res.status(404).send("Not Found");
+
+    // if the note is belong to the user then update it.
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
     }
-  );
 
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
+// Route 4: update a existing note using: DELETE "api/notes/deletenote". Login required.
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    // find the note to be delete and delete it.
+    let note = await Notes.findById(req.params.id);
+    if (!note) return res.status(404).send("Not Found");
+
+    // if the note is belong to the user then delete it.
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note: note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = router;
